ff457ac507137904d0a2b5151de9c4f705025ee4
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2025-08-13 02:23:47 +0530

    Feature Section

--- components/Sections/FeatureSection.js
+++ components/Sections/FeatureSection.js
@@ -69,56 +69,79 @@ export default function FeatureSection() {
   ];
 
   return (
-    <section className="bg-black text-white py-16">
+    <section className="bg-black text-white py-16 ">
       <div className="max-w-[82rem] mx-auto px-6 lg:px-12 grid grid-cols-1 lg:grid-cols-2 gap-8">
-        <div className="flex flex-col justify-start">
-          <h1 className="text-3xl sm:text-4xl  mb-8">
-            We Create New Solutions and Transform Existing Ones with
-            Technologies That Beat Industry-Best Timelines
+        <div className="flex flex-col justify-start space-y-5">
+          <h1 className="text-3xl sm:text-4xl  mb-8 leading-snug">
+            We Create New Solutions and Transform Existing nes with Technologies
+            That Beat Industry-Best Timelines
           </h1>
 
-          <div className="bg-neutral-900 p-6 rounded-xl shadow-lg max-w-sm">
-            <div className="flex flex-grow items-center mb-4">
+          <div className="relative bg-neutral-900 p-10 rounded-xl shadow-lg max-w-auto lg:max-w-md">
+            <div className="absolute inset-0 overflow-hidden">
+              <div
+                className="absolute top-[-20%] w-[70px] h-[150%] bg-white/10 rotate-[20deg] animate-move-right"
+                style={{
+                  left: `-300px`,
+                  animationDelay: `0s`,
+                }}
+              />
+
+              <div
+                className="absolute top-[-20%] w-[70px] h-[150%] bg-white/10 rotate-[20deg] animate-move-right"
+                style={{
+                  left: `100px`,
+                  animationDelay: `0s`,
+                }}
+              />
+            </div>
+            <div className="  items-center mb-4  space-y-5">
               <span className="text-2xl">
                 {" "}
                 <Image
-                  height={20}
-                  width={20}
+                  height={40}
+                  width={40}
                   src="/images/Features/plane.svg"
                 ></Image>
               </span>
-              <h2 className="ml-3 text-lg font-semibold">
+              <h2 className=" text-2xl font-semibold">
                 Fuel Your Digital-First Idea
               </h2>
             </div>
             <p className="text-gray-300 mb-4">
               With 1600+ Transformation Experts
             </p>
-            <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg">
+            <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 mt-5 rounded-lg">
               Innovate With Us
             </button>
           </div>
         </div>
 
-   
-        <div className="lg:h-[28rem] lg:overflow-y-auto pr-2 space-y-6 no-scrollbar ">
+        <div className="lg:h-[40rem] lg:overflow-y-auto pr-2 space-y-6 no-scrollbar ">
           {features.map((feature, index) => (
-            <div key={index} className="border-b border-gray-800 pb-4">
-              <div className="flex items-center mb-2 justify-between group">
-                <div className="flex">
+            <div key={index} className="border-b border-gray-800 pb-4 group">
+              <div className="flex items-center mb-2 justify-between py-4">
+                <div className="flex ">
                   {" "}
                   <span className="text-2xl ">
-                    <Image height={30} width={30} src={feature.icon } className="w-4 h-4 group-hover:animate-moveRight"></Image>
+                    <Image
+                      height={30}
+                      width={30}
+                      src={feature.icon}
+                      className="w-10 h-10 group-hover:animate-moveRight"
+                    ></Image>
                   </span>
-                  <h3 className="ml-3 text-lg font-semibold">
+                  <h3 className="ml-5 text-lg font-semibold">
                     {feature.title}
                   </h3>
                 </div>
                 <div className=" group">
-                  <FaChevronRight className="w-4 h-4 group-hover:animate-moveRight" />
+                  <FaChevronRight className="w-8 h-5 group-hover:animate-moveRight" />
                 </div>
               </div>
-              <p className="text-gray-400 text-sm">{feature.description}</p>
+              <p className=" text-sm pb-5 leading-loose ">
+                {feature.description}
+              </p>
             </div>
           ))}
         </div>
